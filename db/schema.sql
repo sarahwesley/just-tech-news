9bb51b33118675bfad1e75cfd92285242707c770
sarahwesley <92330391+sarahwesley@users.noreply.github.com>
2022-01-10 18:43:06 -0500

    database, apis set up

---
+++ db/schema.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS just_tech_new_db;
+
+CREATE DATABASE just_tech_new_db;
---
+++ models/index.js
@@ -0,0 +1,11 @@
+const User = require('./User');
+
+const router = require('express').Router();
+
+const userRoutes = require('./user-routes.js');
+
+router.use('/users', userRoutes);
+
+module.exports = router;
+
+module.exports = { User };
