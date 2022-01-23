7bec18281416ba672b239822b8ddccc780183033
sarahwesley <92330391+sarahwesley@users.noreply.github.com>
2022-01-23 09:21:39 -0500

    final routes added

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,5 @@
 DROP DATABASE IF EXISTS just_tech_new_db;
 
 CREATE DATABASE just_tech_new_db;
+
+USE just_tech_new_db;
--- models/index.js
+++ models/index.js
@@ -1,11 +1,61 @@
+// import all models
+const Post = require('./Post');
 const User = require('./User');
+const Vote = require('./Vote');
+const Comment = require('./Comment');
 
-const router = require('express').Router();
+// create associations
+User.hasMany(Post, {
+  foreignKey: 'user_id'
+});
 
-const userRoutes = require('./user-routes.js');
+Post.belongsTo(User, {
+  foreignKey: 'user_id'
+});
 
-router.use('/users', userRoutes);
+User.belongsToMany(Post, {
+  through: Vote,
+  as: 'voted_posts',
+  foreignKey: 'user_id'
+});
 
-module.exports = router;
+Post.belongsToMany(User, {
+  through: Vote,
+  as: 'voted_posts',
+  foreignKey: 'post_id'
+});
 
-module.exports = { User };
+Vote.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Vote.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+User.hasMany(Vote, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Vote, {
+  foreignKey: 'post_id'
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Vote, Comment };
+
